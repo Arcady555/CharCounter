6fb92cfa2d840804acc577158a52290be31692ba
Arcady555 <parfenov7233@gmail.com>
2024-01-22 22:19:26 +0300

    Initial commit

---
+++ src/test/java/ru/parfenov/CharCounter/CharCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.parfenov.CharCounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
